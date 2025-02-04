83fcc5b2a67f35ad40fb17b2c90ff9c7735323aa
Kyle Fernandez <fkyleowie@gmail.com>
2025-02-04 13:53:36 +0800

    Initial Commit

---
+++ src/components/UseStateEffectTimer.js
@@ -0,0 +1,19 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react'
+
+function UseStateEffectTimer() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count+1)
+        },1000);
+    });
+    return (
+        <div>
+            <Text>Rendered {count} times!</Text>
+        </div>
+    );
+}
+
+export default UseStateEffectTimer
